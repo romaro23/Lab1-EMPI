3deec5073ceaca8a11083278b5632f3b7b1e3db6
Romaro <Romaro@ROMARO-PC>
2025-02-06 14:29:07 +0200

    Add project files.

---
+++ Lab1/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new Microsoft.Win32.OpenFileDialog
+            {
+                Title = "Open Text File",
+                Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*",
+                Multiselect = false
+            };
+
+            bool? result = openFileDialog.ShowDialog();
+            if (result != true)
+            {
+                return;
+            }
+            var filePath = openFileDialog.FileName;
+            using var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+            using var reader = new StreamReader(stream);
+
+            var col1 = new List<double>();
+            var col2 = new List<double>();
+            while (!reader.EndOfStream)
+            {
+                var line = await reader.ReadLineAsync();
+                if(string.IsNullOrWhiteSpace(line)) continue;
+
+                var parts = line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                if (parts.Length >= 2 && double.TryParse(parts[0], out double value1) &&
+                    double.TryParse(parts[1], out double value2))
+                {
+                    col1.Add(value1);
+                    col2.Add(value2);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
